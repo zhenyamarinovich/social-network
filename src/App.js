6f24e7b612e90a2794bdeea0638d1cb25c82932b
Eugene Marinovich <eugene.m@razortheory.com>
2022-01-31 14:11:26 +0300

    feat: create project, add structure

---
+++ src/App.js
@@ -0,0 +1,8 @@
+import "./App.css";
+import Header from "./features/Header";
+
+function App() {
+  return <Header />;
+}
+
+export default App;
---
+++ src/features/Header/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import style from "./Header.module.scss";
+
+const Header = () => {
+  return (
+    <div className={style.wrapper}>
+      <header className={style.header}></header>
+    </div>
+  );
+};
+
+export default Header;
